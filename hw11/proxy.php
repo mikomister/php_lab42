3e47e098f06e553152b10131bdbeb7eb72efb43b
Michael Kon'kov <m.k-mister@yandex.ru>
2018-05-06 22:37:58 +0300

    Update Task 11

--- hw11/proxy.php
+++ hw11/proxy.php
@@ -5,33 +5,36 @@
  * Date: 06.05.18
  * Time: 19:13
  */
-
+session_save_path(__DIR__ . "/sessions");
+session_name("task11_SID");
+session_set_cookie_params(60);
 session_start();
 
 if (!isset($_SESSION["task11"])) {
     $_SESSION["task11"] = $_POST["strsToJSON"];
 }
 
-//var_dump($_REQUEST);
-function copyHeaders(): string
-{
-    $resultHeaders = "";
-    foreach (getallheaders() as $key => $value) {
-//        if($key == Content-Length)
-        $resultHeaders .= "$key: $value\r\n";
-    }
-    return $resultHeaders;
-}
+//function copyHeaders(): string
+//{
+//    $resultHeaders = "";
+//    foreach (getallheaders() as $key => $value) {
+////        if($key == Content-Length)
+//        $resultHeaders .= "$key: $value\r\n";
+//    }
+//    return $resultHeaders;
+//}
 
 //echo  "<pre>";
 //print_r(copyHeaders());
 
+$cl = strlen($_POST["strsToJSON"]);
 $context_options = array(
     'http' => array(
         'method' => 'POST',
-        'header' => copyHeaders(),
+        'header' => 'Content-type: application/x-www-form-urlencoded' . "Content-Length: $cl",
         'content' => $_POST["strsToJSON"],
     )
 );
 //, stream_context_create($context_options)
-echo file_get_contents("http://127.0.0.1:5000/hw4/Task4.php", false, stream_context_create($context_options));
+$context = stream_context_create($context_options);
+echo file_get_contents("http://localhost:4000", false, $context);
